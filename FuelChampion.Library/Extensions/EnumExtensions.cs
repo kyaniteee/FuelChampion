d1a520ea61af960ac0fd1b9ee0fef242240b9f0e
Zakhar Kolesnychenko <kolesnychenkopl@gmail.com>
2025-01-10 20:42:29 +0100

    enum to list

--- FuelChampion.Library/Extensions/EnumExtensions.cs
+++ FuelChampion.Library/Extensions/EnumExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using FuelChampion.Library.Models;
+using System.ComponentModel;
 using System.Reflection;
 
 namespace FuelChampion.Library.Extensions;
@@ -10,5 +11,12 @@ public static class EnumExtensions
         FieldInfo field = value.GetType().GetField(value.ToString());
         DescriptionAttribute attribute = field.GetCustomAttribute<DescriptionAttribute>();
         return attribute?.Description ?? value.ToString();
+    }
+
+    public static IList<EnumModel<T>> ToList<T>() where T : Enum
+    {
+        var values = Enum.GetValues(typeof(T)) as IList<T>;
+        var result = values.Select(x => new EnumModel<T>(x, x.ToDescription())).ToList();
+        return result;
     }
 }
---
+++ FuelChampion.Library/Models/EnumModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FuelChampion.Library.Models;
+
+public class EnumModel<T> where T : Enum
+{
+    public T? Value { get; set; }
+    public string? Description { get; set; }
+
+    public EnumModel(T? value, string? description)
+    {
+        Value = value;
+        Description = description;
+    }
+}

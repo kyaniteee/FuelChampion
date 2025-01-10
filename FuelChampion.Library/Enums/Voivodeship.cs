672525655e5c082dd830dea673a8654313ea29e7
Zakhar Kolesnychenko <kolesnychenkopl@gmail.com>
2025-01-10 20:22:35 +0100

    voivodeship enum

---
+++ FuelChampion.Library/Enums/Voivodeship.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+
+namespace FuelChampion.Library.Enums;
+
+public enum Voivodeship
+{
+    [Description("Dolnośląskie")]
+    Dolnoslaskie,
+
+    [Description("Kujawsko-Pomorskie")]
+    KujawskoPomorskie,
+
+    [Description("Lubelskie")]
+    Lubelskie,
+
+    [Description("Lubuskie")]
+    Lubuskie,
+
+    [Description("Łódzkie")]
+    Lodzkie,
+
+    [Description("Małopolskie")]
+    Malopolskie,
+
+    [Description("Mazowieckie")]
+    Mazowieckie,
+
+    [Description("Opolskie")]
+    Opolskie,
+
+    [Description("Podkarpackie")]
+    Podkarpackie,
+
+    [Description("Podlaskie")]
+    Podlaskie,
+
+    [Description("Pomorskie")]
+    Pomorskie,
+
+    [Description("Śląskie")]
+    Slaskie,
+
+    [Description("Świętokrzyskie")]
+    Swietokrzyskie,
+
+    [Description("Warmińsko-Mazurskie")]
+    WarminskoMazurskie,
+
+    [Description("Wielkopolskie")]
+    Wielkopolskie,
+
+    [Description("Zachodniopomorskie")]
+    Zachodniopomorskie
+}

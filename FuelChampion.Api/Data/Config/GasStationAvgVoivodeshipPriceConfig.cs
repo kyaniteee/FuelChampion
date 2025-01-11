d9707d7d8d293d00eba5cec209e432358693a8f3
Zakhar Kolesnychenko <kolesnychenkopl@gmail.com>
2025-01-11 11:16:20 +0100

    precyzja 2 dla decimal, żeby nie było warningów

--- FuelChampion.Api/Data/Config/GasStationAvgVoivodeshipPriceConfig.cs
+++ FuelChampion.Api/Data/Config/GasStationAvgVoivodeshipPriceConfig.cs
@@ -10,5 +10,9 @@ public class GasStationAvgVoivodeshipPriceConfig : IEntityTypeConfiguration<GasS
     {
         builder.ToView("GasStationAvgVoivodeshipPrices");
         builder.HasNoKey();
+        builder.Property(c => c.PricePerLiterAvgPb98).HasPrecision(2).HasColumnName("PricePerLiterAvgPb98");
+        builder.Property(c => c.PricePerLiterAvgPb95).HasPrecision(2).HasColumnName("PricePerLiterAvgPb95");
+        builder.Property(c => c.PricePerLiterAvgDiesel).HasPrecision(2).HasColumnName("PricePerLiterAvgDiesel");
+        builder.Property(c => c.PricePerLiterAvgLpg).HasPrecision(2).HasColumnName("PricePerLiterAvgLpg");
     }
 }
